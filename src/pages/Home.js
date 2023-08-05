dd999400a9a41fc0797dfd0d7c51fb59ed0c30f8
Mustafa Yüce <75587873+mustafayucedev@users.noreply.github.com>
2023-08-05 12:14:39 +0300

    responsive grid completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,9 +21,9 @@ function Home() {
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-4 gap-5">
+      <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
         {postShow.map((post,index) => (
-          <div key={index} className="border border-gray-200 rounded-sm mb-5 p-5">
+          <div key={index} className="border border-gray-200 rounded-sm p-5">
             <div className="text-lg font-bold mb-3 h-10 overflow-hidden leading-10"> {post.blogTitle} </div>
             <div className="text-gray-500 leading-6 text-md mb-5"> {post.blogDesc} </div>
             <div className="text-sm text-gray-600">
